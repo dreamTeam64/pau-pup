a833ac92e2f178c77719b6c4415e73d5c04b5a3c
fontaine pierre <pierre.ftn64@gmail.com>
2016-01-12 14:30:14 +0100

    add files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8"/>
+	<title>Pau'pup</title>
+	<link href="style.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body>
+	<div id="global">
+		<div id="menu">
+			<ul>
+				<li><a href="#">Accueuil</a></li>
+				<li><a href="#">Méthodes</a></li>
+				<li><a href="#">PortFolio</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+
+		<div id="wrapper">
+			Vous cherchez des solutions pour augmenter les performances de votre entreprise ?
+		</div>
+
+		<div id="footer">
+			
+		</div>
+	</div>
+</body>
