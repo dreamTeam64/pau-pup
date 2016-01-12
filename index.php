e36ce173b89e03c8e536f771acf798fbe5d54ff1
fontaine pierre <pierre.ftn64@gmail.com>
2016-01-12 19:16:47 +0100

    add some stuff

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset = "utf-8"/>
 	<title>Pau'pup</title>
 	<link href="style.css" rel="stylesheet" type="text/css" media="all">
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
 </head>
 <body>
 	<div id="global">
@@ -20,19 +21,47 @@
 		</div>
 
 		<div id="wrapper">
-			<div id="presentation">
-				<center>Des Services De Qualité ! </center>
+			<div id="presentation" class="section">
+				<center><h3>Des Services De Qualité ! </h3></center>
 				<br>
 				<p>
 					Qui mieux que des étudiants passionnés par leur formation peuvent mettre à profit leurs connaissances ?
 				</p>
-				<br>
 				<p>
 					L'économie et l'Informatique sont les points forts de notre "Organisation" ! Nous sommes aptes à vous
 					aider dans la communication, et à vous épauler sur le côté créatif !
-
-					<img src="img/js.png">
 				</p>
+				<p>
+					Nous tendons à manipuler les dernières technologies pour adapter aux mieux nos solutions !
+					<br><br>
+
+					<img src="img/js.png" height="100" width="100">
+					<img src="img/angular.png" height="100" width="100">
+					<img src="img/css.png" height="100" width="100">
+					<br><br>
+				</p>
+			</div>
+			<hr color="black" width="300px">
+			<div id="profileEnt" class="section">
+				<p>
+					<center><h3>Profile Recherché</h3></center>
+				</p>
+				<p style="inherit">
+					<b><i>Votre identité</i></b>
+					<ul>
+						<li>Professionnel (ex: PME)</li>
+						<li>Particulier (ex: photographe,musicien ...)</li>
+						<li>Association</li>
+					</ul>
+
+					<b><i>Lieux</i></b>
+					<ul>
+						<li>Pau</li>
+						<li>Biarritz</li>
+						<li>Tarbes</li>
+					</ul>
+				</p>
+				
 			</div>
 			
 				
