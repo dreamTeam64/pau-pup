f7093ee3afe7a3bb22bf6e5b32ca5af0f5b2d9b8
fontaine pierre <pierre.ftn64@gmail.com>
2016-01-21 18:48:59 +0100

    adjustment timing

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 $(function(){ //se lance quand la page est chargé
       setInterval(function(){
-         $(".slideshow ul").animate({marginLeft:-100},800,function(){
+         $(".slideshow ul").animate({marginLeft:-100},500,function(){
             $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
          })
       }, 3500);
