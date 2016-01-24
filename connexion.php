92a043520bd11415572ed8317e99091c3cefb739
fontaine pierre <pierre.ftn64@gmail.com>
2016-01-24 12:23:29 +0100

    mise en place de l'intranet

---
+++ connexion.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8"/>
+	<title>Pau'pup</title>
+	<link href="style.css" rel="stylesheet" type="text/css" media="all">
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/slide.js"></script>
+	
+
+
+</head>
+<body>
+	<div id="global">
+		<div id="menu">
+			<div id="Logo">
+				<a href="index.php"><img src="img/LOGO.png" width="250px"></a>
+			</div>
+			<ul>
+				<li>
+					<a href="index.php">
+  							<span class="glyphicon glyphicon-home" aria-hidden="false"> Accueil</span>
+					</a>
+				</li>
+				<li>
+					<a href="equipe.php">
+  							<span class="glyphicon glyphicon-user" aria-hidden="true"> Equipe</span>
+					</a>
+				</li>
+				<li>
+					<a href="methode.php">
+  							<span class="glyphicon glyphicon-pencil" aria-hidden="true"> Méthode</span>
+					</a>
+				</li>
+				<li>
+					<a href="contact.php">
+  							<span class="glyphicon glyphicon-send" aria-hidden="true"> Contact</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div id="wrapper">
+			
+		</div>
+
+		<div id="footer">
+			copyright © 2016. Made with <span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>
+		</div>
+	</div>
+</body>
--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 		<div id="wrapper">
 			<center><h3><span class="glyphicon glyphicon-warning-sign" aria-hidden="false"> Site actuellement en chantier ! Mais nous progressons vite !</h3></span></center>
 			<div class="progress">
-				<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%"></div>
+				<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%"></div>
 			</div>
 			<div id="presentation" class="section">
 				<center><h3>Des Services De Qualité ! </h3></center>
@@ -133,6 +133,8 @@
 
 		<div id="footer">
 			copyright © 2016. Made with <span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>
+			<br>
+			<a href="connexion.php">intranet</a>
 		</div>
 	</div>
 </body>
